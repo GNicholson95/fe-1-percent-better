d48306a364438c6736aca2b07f57bdca5290f6e0
George Nicholson <ga.nicholson@outlook.com>
2023-11-08 22:23:11 +0000

    made landing page ready to implement when authorisation is made from back end

--- 1-percent-better/App.js
+++ 1-percent-better/App.js
@@ -7,11 +7,16 @@ import ProfileHeader from "./components/ProfileHeader";
 import DynamicScreen from "./screens/DynamicScreen";
 import ExerciseList from "./screens/ExerciseList";
 import NavBar from "./Navigators/NavBar";
-
+import LandingPage from "./screens/LandingPage";
 const Stack = createStackNavigator();
 
 function App() {
   return (
+    //  --------- UN-COMMENT BELOW TO VIEW LANDING PAGE
+    // <View style={{ flex: 1 }}>
+    //   <LandingPage/>
+    // </View>
+    //  --------- COMMENT OUT BELOW TO VIEW LANDING PAGE
     <SafeAreaView style={{ flex: 1 }}>
       <NavigationContainer>
         <ProfileHeader />
