f91a603f00d52be44e12a554741f78ad4f603ea7
OzzyCF <o5wa1@hotmail.com>
2023-11-09 10:50:30 +0000

    sort integration complete

---
+++ 1-percent-better/components/Sort.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import RNPickerSelect from "react-native-picker-select";
+const Sort = ({ value, onChange }) => {
+  return (
+    <RNPickerSelect
+      onValueChange={(value) => onChange(value)}
+      items={[
+        { label: "Name A-Z", value: "Name A-Z" },
+        { label: "Name Z-A", value: "Name Z-A" },
+      ]}
+      value={value}
+      style={pickerSelectStyles}
+    />
+  );
+};
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "gray",
+    borderRadius: 4,
+    color: "black",
+    paddingRight: 30,
+    backgroundColor: "white",
+    marginTop: 10,
+  },
+  inputAndroid: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    borderColor: "purple",
+    borderRadius: 8,
+    color: "black",
+    paddingRight: 30,
+    backgroundColor: "white",
+    marginTop: 10,
+  },
+});
+export default Sort;
