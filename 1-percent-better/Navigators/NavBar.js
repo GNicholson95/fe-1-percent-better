7424701eefbbee7d5e207b37bc18f0936efb806b
akramgadir <akram.gadir@gmail.com>
2023-11-10 14:50:19 +0000

    navbar improvements, custom functionality added

--- 1-percent-better/Navigators/NavBar.js
+++ 1-percent-better/Navigators/NavBar.js
@@ -1,34 +1,94 @@
-import * as React from "react";
+import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import DynamicScreen from "../screens/DynamicScreen";
 import ExerciseList from "../screens/ExerciseList";
 import MyExercisesScreen from "../screens/MyExercisesScreen";
 import MySessionsScreen from "../screens/MySessionsScreen";
 import Icon from "react-native-vector-icons/Ionicons";
-import LandingPage from "../screens/LandingPage";
-import WorkoutDetail from "../screens/WorkoutDetail";
+import { View, Text, TouchableOpacity } from "react-native";
 
 const Tab = createMaterialTopTabNavigator();
+
+const CustomTabBar = ({ state, descriptors, navigation }) => {
+  return (
+    <View style={styles.tabBarContainer}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.tabItem,
+              { backgroundColor: isFocused ? "#555" : "#323939" },
+            ]}
+            onPress={onPress}
+            onLongPress={onLongPress}
+          >
+            <Icon
+              name={options.tabBarIconName || "help-circle"}
+              size={20}
+              color={isFocused ? "white" : "grey"}
+            />
+            <Text
+              style={[styles.tabLabel, { color: isFocused ? "white" : "grey" }]}
+            >
+              {options.tabBarLabel}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = {
+  tabBarContainer: {
+    flexDirection: "row",
+    backgroundColor: "#323939",
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: "center",
+    padding: 10,
+  },
+  tabLabel: {
+    marginTop: 0,
+    fontSize: 12,
+  },
+};
 
 function NavBar() {
   return (
     <Tab.Navigator
+      tabBar={(props) => <CustomTabBar {...props} />}
       initialRouteName="Home"
       tabBarPosition="bottom"
       swipeEnabled={true}
       scrollEnabled={true}
-      // initialRouteName="DynamicScreen"
       screenOptions={{
-        tabBarLabelStyle: {
-          fontSize: 8,
-          color: "yellow",
-          flexWrap: "nowrap",
-        },
-        // tabBarLabelStyle: { fontSize: 13, color: "#fd5732" },
-        tabBarStyle: { backgroundColor: "#323939" },
-        tabBarActiveTintColor: "white",
-        tabBarInactiveTintColor: "grey",
-        tabBarShowLabel: true,
+        tabBarLabelStyle: { fontSize: 8, color: "yellow", flexWrap: "nowrap" },
+        tabBarShowLabel: false,
       }}
     >
       <Tab.Screen
@@ -36,9 +96,7 @@ function NavBar() {
         component={DynamicScreen}
         options={{
           tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="home" color={color} size={size} />
-          ),
+          tabBarIconName: "home",
         }}
       />
       <Tab.Screen
@@ -46,29 +104,23 @@ function NavBar() {
         component={ExerciseList}
         options={{
           tabBarLabel: "All Exercises",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="barbell-outline" color={color} size={size} />
-          ),
+          tabBarIconName: "barbell-outline",
         }}
       />
       <Tab.Screen
         name="MyExercises"
-        component={MyExercisesScreen} // My Exercises
+        component={MyExercisesScreen}
         options={{
           tabBarLabel: "My Exercises",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="person-outline" color={color} size={size} />
-          ),
+          tabBarIconName: "person-outline",
         }}
       />
       <Tab.Screen
         name="MySessions"
-        component={MySessionsScreen} // My Sessions
+        component={MySessionsScreen}
         options={{
           tabBarLabel: "My Sessions",
-          tabBarIcon: ({ color, size }) => (
-            <Icon name="bar-chart-outline" color={color} size={size} />
-          ),
+          tabBarIconName: "fitness-outline",
         }}
       />
     </Tab.Navigator>
