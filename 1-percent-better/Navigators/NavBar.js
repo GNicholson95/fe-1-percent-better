0e4b89c2167f02b604535dac6657a135b473288c
akramgadir <akram.gadir@gmail.com>
2023-11-10 14:04:41 +0000

    nav bar sliding updated

--- 1-percent-better/Navigators/NavBar.js
+++ 1-percent-better/Navigators/NavBar.js
@@ -4,38 +4,72 @@ import DynamicScreen from "../screens/DynamicScreen";
 import ExerciseList from "../screens/ExerciseList";
 import MyExercisesScreen from "../screens/MyExercisesScreen";
 import MySessionsScreen from "../screens/MySessionsScreen";
+import Icon from "react-native-vector-icons/Ionicons";
+import LandingPage from "../screens/LandingPage";
+import WorkoutDetail from "../screens/WorkoutDetail";
 
 const Tab = createMaterialTopTabNavigator();
 
 function NavBar() {
   return (
     <Tab.Navigator
-      initialRouteName='DynamicScreen'
+      initialRouteName="Home"
+      tabBarPosition="bottom"
+      swipeEnabled={true}
+      scrollEnabled={true}
+      // initialRouteName="DynamicScreen"
       screenOptions={{
-        tabBarLabelStyle: { fontSize: 13, color: "#fd5732" },
-        tabBarStyle: { backgroundColor: "#393939" },
+        tabBarLabelStyle: {
+          fontSize: 8,
+          color: "yellow",
+          flexWrap: "nowrap",
+        },
+        // tabBarLabelStyle: { fontSize: 13, color: "#fd5732" },
+        tabBarStyle: { backgroundColor: "#323939" },
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "grey",
+        tabBarShowLabel: true,
       }}
     >
       <Tab.Screen
-        name='DynamicScreen'
+        name="DynamicScreen"
         component={DynamicScreen}
-        options={{ tabBarLabel: "Home" }}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="home" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
-        name='ExerciseList'
+        name="ExerciseList"
         component={ExerciseList}
-        options={{ tabBarLabel: "All Exercises" }}
+        options={{
+          tabBarLabel: "All Exercises",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="barbell-outline" color={color} size={size} />
+          ),
+        }}
       />
-
       <Tab.Screen
-        name='MyExercises'
+        name="MyExercises"
         component={MyExercisesScreen} // My Exercises
-        options={{ tabBarLabel: "My Exercises" }} // Label for the tab
+        options={{
+          tabBarLabel: "My Exercises",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="person-outline" color={color} size={size} />
+          ),
+        }}
       />
       <Tab.Screen
-        name='MySessions'
+        name="MySessions"
         component={MySessionsScreen} // My Sessions
-        options={{ tabBarLabel: "My Sessions" }} // Label for the tab
+        options={{
+          tabBarLabel: "My Sessions",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="bar-chart-outline" color={color} size={size} />
+          ),
+        }}
       />
     </Tab.Navigator>
   );
