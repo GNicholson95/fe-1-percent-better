e0ef33c96177fd92743622649a26cf99dfd54a33
libertyskies <libertyemerson@outlook.com>
2023-11-13 09:28:27 +0000

    adding a delete exercise service

---
+++ 1-percent-better/services/DeleteExercise.js
@@ -0,0 +1,38 @@
+import graphqlAPI from "./graphqlClient";
+
+export const DELETE_EXERCISE_MUTATION = `
+  mutation deleteExercise(
+    $exerciseId:ID!
+  ) {
+    deleteExercise(
+    exerciseId:$exerciseId)
+    {
+      exercise 
+      {
+        exerciseId
+      }
+   }
+}`;
+
+export const deleteExercise = async (exerciseId) => {
+  try {
+    const response = await graphqlAPI({
+      data: {
+        query: DELETE_EXERCISE_MUTATION,
+        variables: {
+          exerciseId: exerciseId,
+        },
+      },
+    });
+
+    if (response.data.errors) {
+      return [];
+    }
+
+    const isDeletedExercise = response.data.data.deleteExercise == null;
+
+    return isDeletedExercise; // will return true if deleted or false if not
+  } catch (error) {
+    throw error;
+  }
+};
