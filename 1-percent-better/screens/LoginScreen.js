2f8314a2613f8b585db0330f5920572a6bf80f08
libertyskies <libertyemerson@outlook.com>
2023-11-14 15:05:17 +0000

    Revert "resolving merge conflict"
    This reverts commit ba73adee76e6d403e8e135cc630b46a1a02b1d9f.

--- 1-percent-better/screens/LoginScreen.js
+++ 1-percent-better/screens/LoginScreen.js
@@ -1,22 +1,50 @@
+<<<<<<< HEAD
+import axios from "axios";
 import React, { useState } from "react";
+=======
+import React, { useContext, useState } from "react";
+>>>>>>> main
 import {
   View,
   Text,
   TextInput,
-  Image,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
+import { TokenAuth, isLoggedIn } from "../services/LogIn";
+import { useUserContext } from "../context/UserContext";
+import Toast from "react-native-root-toast";
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const { user, setUser } = useUserContext();
 
-  const handleLogin = () => {
-    console.log("Username:", username);
-    console.log("Password:", password);
-    console.log("Email:", email);
+  const handleLogin = async () => {
+    try {
+      const token = await TokenAuth(username, password);
+      const user = await isLoggedIn(token);
+      await setUser(Number(user));
+      Toast.show("Log in success!", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0,
+      });
+      navigation.navigate("DynamicScreen");
+    } catch (error) {
+      Toast.show("Log in failed!", {
+        duration: Toast.durations.SHORT,
+        position: Toast.positions.BOTTOM,
+        shadow: true,
+        animation: true,
+        hideOnPress: true,
+        delay: 0,
+      });
+      console.error(error, "error");
+    }
   };
 
   return (
@@ -41,9 +69,8 @@ const LoginScreen = () => {
       <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
         <Text style={styles.loginButtonText}>Login</Text>
       </TouchableOpacity>
-      {/* <TouchableOpacity>
-        <Text style={styles.text}>Forgotten your password?</Text>
-      </TouchableOpacity> */}
+
+      {error ? <Text style={styles.errorText}>{error}</Text> : null}
     </View>
   );
 };
@@ -84,6 +111,11 @@ const styles = StyleSheet.create({
     color: "white",
     fontWeight: "bold",
     fontSize: 18,
+  },
+  errorText: {
+    color: "red",
+    marginTop: 10,
+    textAlign: "center",
   },
 });
 
